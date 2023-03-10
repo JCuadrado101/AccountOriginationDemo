877b18efd0a9968cabc641b76df495b4af7eff33
Jordan Cuadrado <jcuadrado@psecu.com>
2023-03-10 15:13:04 -0500

    joint account

--- API/SendSMS.cs
+++ API/SendSMS.cs
@@ -17,7 +17,7 @@ namespace Account_Origination_Demo.API
             TwilioClient.Init(accountSid, authToken);
 
             var message = MessageResource.Create(
-                body: $"Hello from PSECU! \n\n{primaryName} would like to add you to their account as an additional person. \n\nClick on this link to get started: 'https://heythere.com' \n\nIf you did not expect this message nor know who this individual is - please ignore this message. ",
+                body: $"Hello from PSECU! \n\n{primaryName} would like to add you to their account as an additional person. \n\nClick on this link to get started: 'accountoriginationdemo.azurewebsites.net/getstartedjoint' \n\nIf you did not expect this message nor know who this individual is - please ignore this message. ",
                 from: new Twilio.Types.PhoneNumber("+18552459202"),
                 to: new Twilio.Types.PhoneNumber($"+1{phoneNumber}")
             );
